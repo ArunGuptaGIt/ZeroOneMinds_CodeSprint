af535b59f90de60c54522c0fd680d1833f166fd0
utkristha <utkristhabhattarai@gmail.com>
2025-07-01 10:23:33 +0545

    added signup views and urls

--- hackathon/api/views.py
+++ hackathon/api/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view,parser_classes
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
 
 from . import models
-from . import seriallizers
+from . import serializers
+
+@api_view(['POST'])
+@parser_classes([MultiPartParser, FormParser])
+def user_signup(request):
+    if request.method == 'POST':
+        serializer = serializers.UserSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status= 201)
+        return Response(serializer.errors,status = 400)
 
 
 
