a8381f137c21f7ef73e758dbbb9ed92902bcbece
utkristha <utkristhabhattarai@gmail.com>
2025-07-01 10:48:42 +0545

    variables named to python standard

--- hackathon/api/urls.py
+++ hackathon/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/',views.user_signup,name='')
+    path('signup/',views.UserSignup,name=''),
+    path('storage/',views.StorageMethods,name=''),
+
 ]
--- hackathon/api/views.py
+++ hackathon/api/views.py
@@ -7,13 +7,17 @@ from . import serializers
 
 @api_view(['POST'])
 @parser_classes([MultiPartParser, FormParser])
-def user_signup(request):
-    if request.method == 'POST':
-        serializer = serializers.UserSerializer(data = request.data)
+def UserSignup(request):
+    serializer = serializers.UserSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data,status= 201)
+    return Response(serializer.errors,status = 400)
+
+@api_view(['POST'])
+def StorageMethods(request):
+    if request.method == "POST":
+        serializer = serializers.StorageSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status= 201)
-        return Response(serializer.errors,status = 400)
-
-
-
+            return Response(serializer.data,)
